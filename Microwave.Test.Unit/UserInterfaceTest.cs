e773eecd1ca788b54eb816a9c30472f7081f6fe7
Moheb420 <68193758+Moheb420@users.noreply.github.com>
2022-11-23 16:09:54 +0100

    error removed from test

--- Microwave.Test.Unit/UserInterfaceTest.cs
+++ Microwave.Test.Unit/UserInterfaceTest.cs
@@ -16,7 +16,7 @@ namespace Microwave.Test.Unit
         private IButton startCancelButton;
 
         private IDoor door;
-
+        private IBuzzer buzzer;
         private IDisplay display;
         private ILight light;
 
@@ -32,13 +32,14 @@ namespace Microwave.Test.Unit
             light = Substitute.For<ILight>();
             display = Substitute.For<IDisplay>();
             cooker = Substitute.For<ICookController>();
-
-            //uut = new UserInterface(
-            //    powerButton, timeButton, startCancelButton,
-            //    door,
-            //    display,
-            //    light,
-            //    cooker);
+            buzzer = Substitute.For<IBuzzer>();
+            uut = new UserInterface(
+                powerButton, timeButton, startCancelButton,
+                door,
+                buzzer,
+                display,
+                light,
+                cooker);
         }
 
         [Test]
