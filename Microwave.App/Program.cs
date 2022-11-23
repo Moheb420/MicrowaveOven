d759295ed7d8ccc8680928d7de7d69d0b38c464d
Moheb420 <68193758+Moheb420@users.noreply.github.com>
2022-11-23 15:18:06 +0100

    merged

--- Microwave.App/Program.cs
+++ Microwave.App/Program.cs
@@ -23,6 +23,7 @@ namespace Microwave.App
             Light light = new Light(output);
 
             Microwave.Classes.Boundary.Timer timer = new Timer();
+            powerTube.changePowerTubeValue(800);
 
             CookController cooker = new CookController(timer, display, powerTube);
 
